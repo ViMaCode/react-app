6070b3815899d11debb36fa29f6cce485c1a4a3a
Vitaliy <vialmacode@gmail.com>
2026-01-03 12:35:50 +0300

    fix: set app theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -7,11 +7,11 @@ interface useThemeResult {
 }
 
 export function useTheme(): useThemeResult {
-	const { theme, setTheme } = useContext(ThemeContext);
+	const { theme = Theme.LIGHT, setTheme } = useContext(ThemeContext);
 
 	const toggleTheme = () => {
 		const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-		setTheme(newTheme);
+		setTheme?.(newTheme);
 		localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
 	};
 
